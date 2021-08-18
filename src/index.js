364a75ad56ea6b9e425aa0c1438a2494baaac765
Victor Otieno <victorotieno598@gmail.com>
2021-08-18 23:00:45 +0300

    Add clear function to remove completed tasks

--- src/index.js
+++ src/index.js
@@ -100,3 +100,15 @@ addNewTodo.addEventListener('keypress', (e) => {
     addNewItem();
   }
 });
+
+clearCompleted.addEventListener('click', () => {
+  const todosArray = JSON.parse(localStorage.getItem('todos'));
+  const itemsToBeDeleted = todosArray.filter((val) => val.completed);
+  itemsToBeDeleted.forEach((val) => {
+    const index = todosArray.indexOf(val);
+    todosArray.splice(index, 1);
+    localStorage.setItem('todos', JSON.stringify(todosArray));
+  });
+  todoList.innerHTML = '';
+  diplayTodoList();
+});
