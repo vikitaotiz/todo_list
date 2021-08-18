d2497ffb072636079fe0731e88dcac23a5cdac33
Victor Otieno <victorotieno598@gmail.com>
2021-08-18 19:05:46 +0300

    Add display function for the todos

--- src/index.js
+++ src/index.js
@@ -6,3 +6,34 @@ const todoList = document.querySelector('#todoList');
 const clearCompleted = document.querySelector('#clearCompleted');
 
 const todos = [];
+
+function diplayTodoList() {
+  const data = localStorage.getItem('todos');
+
+  if (data) {
+    JSON.parse(data).forEach((val) => {
+      const li = document.createElement('li');
+      li.classList.add('list-group-item');
+      // li.setAttribute('draggable', true);
+
+      const text = `
+      <div class="d-flex dragTodo" id="${val.index}" draggable="true">
+        <input class="form-check-input checkTodo" type="checkbox" value=""
+        ${val.completed ? 'checked' : ''}>
+        <div class="fw-bold flex-grow-1 ${val.completed ? 'completeTask' : ''}"
+        id="editDescription"
+        contenteditable="${!val.completed}">
+          ${val.description}
+        <span class="drag"></span>
+        </div>
+         <i class="fas fa-trash deleteBtn m-2"></i>
+         <i class="fas fa-grip-vertical m-2 drag"></i>
+      </div>`;
+
+      li.innerHTML = text;
+      todoList.appendChild(li);
+    });
+  }
+}
+
+diplayTodoList()
