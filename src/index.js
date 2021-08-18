a9598c3315ac0ebe647c0cb9e681bfd05519148b
Victor Otieno <victorotieno598@gmail.com>
2021-08-18 22:59:02 +0300

    Add edit description  function

--- src/index.js
+++ src/index.js
@@ -65,6 +65,21 @@ function diplayTodoList() {
       }
     });
   });
+
+  const editBtns = document.querySelectorAll('#editDescription');
+  editBtns.forEach((btn) => {
+    btn.addEventListener('keypress', (e) => {
+      if (e.key === 'Enter' && e.target.textContent) {
+        const todosArray = JSON.parse(localStorage.getItem('todos'));
+        const todoObject = todosArray.find((todo) => todo.index === Number(e.target.parentNode.id));
+        const index = todosArray.indexOf(todoObject);
+        todosArray[index].description = e.target.textContent;
+        localStorage.setItem('todos', JSON.stringify(todosArray));
+        todoList.innerHTML = '';
+        diplayTodoList();
+      }
+    });
+  });
 }
 
 diplayTodoList();
