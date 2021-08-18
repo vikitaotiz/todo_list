6d8c91b42b296d0a4665d862aaaa154201ee07c1
Victor Otieno <victorotieno598@gmail.com>
2021-08-18 19:12:21 +0300

    Add complete todo function using checkbox in the js file

--- src/index.js
+++ src/index.js
@@ -48,6 +48,26 @@ function diplayTodoList() {
       diplayTodoList();
     });
   });
+
+  // complete task functionality
+  const checkTodos = document.querySelectorAll('.checkTodo');
+  checkTodos.forEach((check) => {
+    check.addEventListener('change', (e) => {
+      const todosArray = JSON.parse(localStorage.getItem('todos'));
+      const todoObject = todosArray.find((todo) => todo.index === Number(e.target.parentNode.id));
+      const index = todosArray.indexOf(todoObject);
+
+      if (!todosArray[index].completed) {
+        todosArray[index].completed = 1;
+        e.target.parentNode.children[1].classList.add('completeTask');
+        localStorage.setItem('todos', JSON.stringify(todosArray));
+      } else {
+        todosArray[index].completed = 0;
+        e.target.parentNode.children[1].classList.remove('completeTask');
+        localStorage.setItem('todos', JSON.stringify(todosArray));
+      }
+    });
+  });
 }
 
 diplayTodoList();
